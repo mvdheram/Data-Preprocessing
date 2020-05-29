e372a0ad083de74b2eb0875929876ad04df41a28
Meher Vivek <vivekdheram@gmail.com>
2020-05-29 21:40:47 +0200

    Adding ANN files

--- 1. Data Preprocessing Template/data_preprocessing_tools.py
+++ 1. Data Preprocessing Template/data_preprocessing_tools.py
@@ -1,19 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 29 18:55:51 2020
-
-@author: Meher
-"""
-
 # Data Preprocessing Tools
 
 # Importing the libraries
 import numpy as np
-import tensorflow as tf
+import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
+dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 print(X)
